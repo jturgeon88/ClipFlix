1ddcb069e270f8343bfc3c40aa5ba5ce8ba3840a
Josh Turgeon <jturgeon88@gmail.com>
2018-11-28 14:00:56 -0600

    Create MovieTile component and refactor Movies to delegate to it

--- src/app/movies/movies.component.ts
+++ src/app/movies/movies.component.ts
@@ -10,8 +10,6 @@ export class MoviesComponent implements OnInit {
 
   movies = MOVIES;
 
-  posterUrl: string = "https://image.tmdb.org/t/p/w440_and_h660_bestv2/";
-
   constructor() { }
 
   ngOnInit() {
