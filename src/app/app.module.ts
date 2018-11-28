26d4245a575c7eb21bb3488dba809a2566cdea64
Josh Turgeon <jturgeon88@gmail.com>
2018-11-28 16:35:26 -0600

    Generate MovieDetailComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { MoviesModule } from './movies/movies.module';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MovieDetailComponent,
   ],
   imports: [
     BrowserModule,
