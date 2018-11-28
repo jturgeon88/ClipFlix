46815c761517295ba237698536f7460129a0a93f
Josh Turgeon <jturgeon88@gmail.com>
2018-11-28 12:22:21 -0600

    add optional syntax

--- src/app/movie.ts
+++ src/app/movie.ts
@@ -1,16 +1,16 @@
 export class Movie {
-    vote_count: any;
+    vote_count?: any;
     id: number;
-    video: any;
-    vote_average: any;
+    video?: any;
+    vote_average?: any;
     title: string;
-    popularity: any;
+    popularity?: any;
     poster_path: string;
-    original_language: any;
-    original_title: any;
-    genre_ids: any[];
-    backdrop_path: any;
-    adult: boolean;
-    overview: any;
-    release_date: any;
+    original_language?: any;
+    original_title?: any;
+    genre_ids?: any[];
+    backdrop_path?: any;
+    adult?: boolean;
+    overview?: any;
+    release_date?: any;
 }
